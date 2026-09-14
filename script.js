93ce2dd62926043c547886a9cca1889b9591ace1
Th3SidEffect <siddharthsingh12128@gmail.com>
2026-09-14 13:11:28 +0530

    Added LocalStorage to store data

--- script.js
+++ script.js
@@ -2,14 +2,46 @@ const taskInput = document.getElementById("taskinput");
 const newtask = document.getElementById("newtask");
 const taskCard = document.getElementById("taskcard");
 let tasks = [];
+localStorage.clear();
+
+
+function oldTaskLoader(task){
+    const input = document.createElement("div");
+    const done = document.createElement("button");
+    const mainCard = document.createElement("div");
+    const del = document.createElement("button");
+    taskCard.appendChild(mainCard)
+    mainCard.appendChild(input);
+    mainCard.appendChild(done);
+    mainCard.appendChild(del);
+    mainCard.id = "maincard"
+    del.textContent = "X";
+    del.id = "delete";
+    done.id = "done";
+    done.textContent = "✓";
+    input.id = "inputdiv";
+    input.textContent = task;
+
+    done.addEventListener("click", 
+        () => {
+            input.style.backgroundColor = "gray";
+            input.style.transform = "translateY(0px)";
+            input.style.boxShadow = "none";
+        }
+    )
+}
 
 newtask.addEventListener("click", 
     () => {
-        if (taskInput.value !== ""){ 
+        if (taskInput.value !== ""){
            const input = document.createElement("div");
            const done = document.createElement("button");
            const mainCard = document.createElement("div");
            const del = document.createElement("button");
+           taskCard.appendChild(mainCard)
+           mainCard.appendChild(input);
+           mainCard.appendChild(done);
+           mainCard.appendChild(del);
            mainCard.id = "maincard"
            del.textContent = "X";
            del.id = "delete";
@@ -17,10 +49,8 @@ newtask.addEventListener("click",
            done.textContent = "✓";
            input.id = "inputdiv";
            input.textContent = taskInput.value;
-           taskCard.appendChild(mainCard)
-           mainCard.appendChild(input);
-           mainCard.appendChild(done);
-           mainCard.appendChild(del);
+           tasks.push(taskInput.value);
+           localStorage.setItem("tasks", JSON.stringify(tasks));
            taskInput.value = "";
 
            done.addEventListener("click", 
@@ -43,3 +73,14 @@ newtask.addEventListener("click",
     }
 )
 
+window.addEventListener("DOMContentLoaded", 
+    () => {
+        const savedTasks = localStorage.getItem("tasks");
+
+        tasks = savedTasks ? JSON.parse(savedTasks) : [];
+
+        tasks.forEach(task => {
+            oldTaskLoader(task);
+        })
+    }
+)
